b035cf3f664757c86ffedd0b15c817fceccf0932
SK_DATA_ENGINEER <64856267+skaba89@users.noreply.github.com>
2026-09-07 22:41:25 +0200

    chore(web): expose shared institutional UI components

---
+++ apps/web/app/_shared/index.ts
@@ -0,0 +1,6 @@
+export { default as Breadcrumbs } from './Breadcrumbs';
+export { default as Button } from './Button';
+export { default as Dialog } from './Dialog';
+export { default as FilterBar, FilterField } from './FilterBar';
+export { default as ResponsiveTable } from './ResponsiveTable';
+export { ToastProvider, useToast } from './Toast';
